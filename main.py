968ec88d0fc7391000fbd8da25581c2be888a679
FelipeCMBarros <108955121+FelipeCMBarros@users.noreply.github.com>
2025-05-14 16:28:01 -0300

    Update main.py
    arrumei problema da tela

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
+import sys
 from pygame.locals import *
  
 pygame.init()
 vec = pygame.math.Vector2  #pra jogos 2d
 
-w = 450 #largura
-h = 400 #altura
+w = 400 #largura
+h = 450 #altura
 
 #Elementos da fisica do jogo
 aceleracao = 0.5
 friccao = -0.12
+
 FPS = 60
  
 FramePerSec = pygame.time.Clock()
@@ -21,13 +23,19 @@ pygame.display.set_icon(pygame.image.load('icon.png'))
 class DonkeyKong(pygame.sprite.Sprite): # Donkey Kong eh a classe do player
     def __init__(self):
         super().__init__() 
+
         self.surf = pygame.Surface((30, 30))
         self.surf.fill((128,255,40))
         self.rect = self.surf.get_rect(center = (10, 420))
+
+        self.pos = vec((10, 385))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
  
 class plataforma(pygame.sprite.Sprite): # classe para as plataformas do jogo
     def __init__(self):
         super().__init__()
+
         self.surf = pygame.Surface((w, 20))
         self.surf.fill((255,0,0))
         self.rect = self.surf.get_rect(center = (w/2, h - 10))
@@ -39,11 +47,12 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(PT1)
 all_sprites.add(P1)
  
+# game loop principal
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit() # para o  codigo para não quebrar como o resto do game loop vindo dps
      
     displaysurface.fill((0,0,0))
  
